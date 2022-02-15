11410322854941231d581da0ba2fbf4b27a737d4
Abednego Aginam <71936420+Abednego-py@users.noreply.github.com>
2022-02-15 21:25:08 +0100

    Added an app bar

--- app/src/main/java/com/example/notepad/NoteDetailsFragment.kt
+++ app/src/main/java/com/example/notepad/NoteDetailsFragment.kt
@@ -2,15 +2,18 @@ package com.example.notepad
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.notepad.data.Note
 import com.example.notepad.databinding.FragmentNoteDetailsBinding
 import com.example.notepad.viewModels.NoteViewModel
 import com.example.notepad.viewModels.NoteViewModelFactory
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class NoteDetailsFragment : Fragment() {
@@ -39,19 +42,84 @@ class NoteDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        args.customNote.userText = binding.note.text.toString()
-        binding.time.text = args.customNote.time
-        args.customNote.title = binding.title.text.toString()
+        viewModel.retrieveItem(args.customNote.id).observe(
+            this.viewLifecycleOwner
+        ) { note ->
+            bind(note)
+        }
 
         binding.check.setOnClickListener {
-            val updatedNote = Note(
-                userText = binding.note.text.toString(),
-                title = binding.title.text.toString(),
-                time = binding.time.text.toString()
-            )
-            viewModel.updateNote(updatedNote)
+
+            val userText = binding.notes.text.toString()
+            val title = binding.title.text.toString()
+            val time = binding.time.text.toString()
+
+            viewModel.updatedNote(title, time, userText, args.customNote.id)
+
+            val action = NoteDetailsFragmentDirections.actionNoteDetailsFragmentToListFragment()
+            findNavController().navigate(action)
         }
     }
 
+    /**
+     * Displays an alert dialog to get the user's confirmation before deleting the item.
+     */
+    private fun showConfirmationDialog() {
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(android.R.string.dialog_alert_title))
+            .setMessage("Do you want to delete this note?")
+            .setCancelable(false)
+            .setNegativeButton("No") { _, _ -> }
+            .setPositiveButton("Yes") { _, _ ->
+                deleteItem()
+            }
+            .show()
+    }
 
+    /**
+     * Deletes the current item and navigates to the list fragment.
+     */
+    private fun deleteItem() {
+        viewModel.retrieveItem(args.customNote.id).observe(
+            this.viewLifecycleOwner
+        ) { note ->
+            viewModel.deleteNote(note)
+        }
+        findNavController().navigateUp()
+    }
+
+    private fun bind(note: Note) {
+        binding.apply {
+            title.setText(note.title)
+            time.text = note.time
+            notes.setText(note.userText)
+        }
+    }
+
+    /**
+     * Called before fragment is destroyed.
+     */
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        // Hide keyboard.
+//        val inputMethodManager = requireActivity().getSystemService(INPUT_METHOD_SERVICE) as
+//                InputMethodManager
+//        inputMethodManager.hideSoftInputFromWindow(requireActivity().currentFocus?.windowToken, 0)
+//        _binding = null
+//    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.delete -> {
+                showConfirmationDialog()
+                true
+            }
+            R.id.app_bar_search -> {
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
 }
